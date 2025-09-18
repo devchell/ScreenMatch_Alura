2903bd31a104ad239e52e783a9e22cf8c3f3eaec
Chell <rodriguessnts@outlook.com>
2025-09-18 20:15:49 -0300

    adicionando classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
     public static void main(String[] args) {
 
+        Filme filme1 = new Filme();
+        filme1.nome = "Meu Malvado Favorito";
+        filme1.anoLancamento = 2010;
+        filme1.duracaoMinutos = 95;
+        filme1.incluidoPremium = true;
+
+        filme1.avaliaFilme(8);
+        filme1.avaliaFilme(7);
+        filme1.avaliaFilme(9);
+        filme1.exibeFichaTecnica();
+
     }
+
 }
